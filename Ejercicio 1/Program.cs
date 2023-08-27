04ebddc944749e188deb44ab7d6f694b1f7bc09e
José Manuel <Jos Manuel@DESKTOP-L67G7N4>
2023-08-27 15:45:21 -0500

    Agregar archivos de proyecto.

---
+++ Ejercicio 1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+{
+    float c=0.0f,f=0.0f;
+    Console.Write("Ingrese la Temperatura en grados Fahrenheit: ");
+    f = Convert.ToSingle(Console.ReadLine());
+    c = (f - 32) * 5 / 9;
+    Console.WriteLine("La temperatura equivalente de {0} grados Fahrenheit en Celsius es: {1}", f, c);
+    Console.ReadKey();
+}
